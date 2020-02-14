32ee07020ab54b96918dd497bdb7435c59bfaf79
marwin.torrez <marwin.torrez@truextend.com>
2020-02-14 16:58:36 -0400

    first commit

---
+++ src/main/java/com/TXP1/TX_Problem1/TxProblem1Application.java
@@ -0,0 +1,13 @@
+package com.TXP1.TX_Problem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxProblem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxProblem1Application.class, args);
+	}
+
+}
